c0af0c5d944761e623a9c6a514f3e0aba064e993
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-08-25 17:37:33 -0400

    Create post route that adds a budget and its lines

--- api/budgets/budgetsModel.js
+++ api/budgets/budgetsModel.js
@@ -2,15 +2,17 @@ const db = require('../../data/dbConfig');
 
 module.exports = {
     getByUserId,
-    getById,
+    getLinesById,
     remove,
+    addBudget,
+    addBudgetLines,
 };
 
 function getByUserId(id) {
     return db('budget_names').where('user_id', id);
 }
 
-function getById(id) {
+function getLinesById(id) {
     return db('stored_budget_lines as s')
         .join('categories as c', 'c.category_id', 's.category_id')
         .select('s.line_id', 's.amount', 'c.category_name')
@@ -21,4 +23,22 @@ function remove(id) {
     return db('budget_names')
         .where({ budget_name_id: id })
         .delete();
+}
+
+function findById(id) {
+    return db('budget_names')
+        .where('budget_name_id', id)
+        .first();
+}
+
+function addBudget(budget) {
+    return db('budget_names')
+        .insert(budget)
+        .then(([id]) => findById(id));
+}
+
+function addBudgetLines(lines, id) {
+    return db('stored_budget_lines')
+        .insert(lines)
+        .then(() => getLinesById(id));
 }
--- api/budgets/budgetsRouter.js
+++ api/budgets/budgetsRouter.js
@@ -22,14 +22,33 @@ router.get('/:id/savedBudgets', async (req, res, next) => {
 router.get('/:id/savedBudgets/:budgetID', async (req, res, next) => {
     try {
         const id = req.params.budgetID;
-        const budget = await Budgets.getById(id);
+        const budget = await Budgets.getLinesById(id);
         res.status(200).json(budget);
     } catch (err) {
         next({ err, stat: 500, message: 'Error getting a specific budget.' });
     }
 });
 
-router.post('/:id/savedBudgets', async (req, res, next) => {});
+router.post('/:id/savedBudgets', async (req, res, next) => {
+    try {
+        const budget_name = req.body.budget_name;
+        const lines = req.body.lines;
+        const user_id = req.params.id;
+        const newBudget = { budget_name, user_id };
+        const createdBudget = await Budgets.addBudget(newBudget);
+        lines.forEach(line => {
+            line.user_id = +user_id;
+            line.budget_name_id = createdBudget.budget_name_id;
+        });
+        const fullBudget = await Budgets.addBudgetLines(
+            lines,
+            createdBudget.budget_name_id
+        );
+        res.status(200).json(fullBudget);
+    } catch (err) {
+        next({ err, stat: 500, message: 'Error creating a new budget.' });
+    }
+});
 
 router.delete('/:id/savedBudgets/:budgetID', async (req, res, next) => {
     try {
@@ -41,6 +60,11 @@ router.delete('/:id/savedBudgets/:budgetID', async (req, res, next) => {
     }
 });
 
-router.put('/:id/savedBudgets/:budgetID', async (req, res, next) => {});
+router.put('/:id/savedBudgets/:budgetID', async (req, res, next) => {
+    try {
+    } catch (err) {
+        next({ err, stat: 500, message: 'Error editing a budget.' });
+    }
+});
 
 module.exports = router;

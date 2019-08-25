05df9062686d41302d76ac842db87cb55731c5dc
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-08-25 17:18:18 -0400

    Create delete route for budget

--- api/budgets/budgetsModel.js
+++ api/budgets/budgetsModel.js
@@ -3,6 +3,7 @@ const db = require('../../data/dbConfig');
 module.exports = {
     getByUserId,
     getById,
+    remove,
 };
 
 function getByUserId(id) {
@@ -13,5 +14,11 @@ function getById(id) {
     return db('stored_budget_lines as s')
         .join('categories as c', 'c.category_id', 's.category_id')
         .select('s.line_id', 's.amount', 'c.category_name')
-        .where('s.budget_name_id', id)        
+        .where('s.budget_name_id', id);
+}
+
+function remove(id) {
+    return db('budget_names')
+        .where({ budget_name_id: id })
+        .delete();
 }
--- api/budgets/budgetsRouter.js
+++ api/budgets/budgetsRouter.js
@@ -9,32 +9,38 @@ const Budgets = require('./budgetsModel');
 //router.use('/:id/savedBudgets/:budgetID, validBudgetID)
 
 router.get('/:id/savedBudgets', async (req, res, next) => {
-    try{
+    try {
         const id = req.params.id;
-        const budgets = await Budgets.getByUserId(id)
-        let budgetsToReturn = budgets.length<2 ? budgets[0] : budgets
+        const budgets = await Budgets.getByUserId(id);
+        let budgetsToReturn = budgets.length < 2 ? budgets[0] : budgets;
         res.status(200).json(budgetsToReturn);
-    } catch(err){
-        next({err, stat: 500, message: 'Error getting saved budgets.'})
+    } catch (err) {
+        next({ err, stat: 500, message: 'Error getting saved budgets.' });
     }
 });
 
 router.get('/:id/savedBudgets/:budgetID', async (req, res, next) => {
-    try{
-        const id = req.params.budgetID
-        const budget = await Budgets.getById(id)
+    try {
+        const id = req.params.budgetID;
+        const budget = await Budgets.getById(id);
         res.status(200).json(budget);
-    }catch(err){
-        next({err, stat: 500, message: 'Error getting a specific budget.'})
+    } catch (err) {
+        next({ err, stat: 500, message: 'Error getting a specific budget.' });
     }
 });
 
 router.post('/:id/savedBudgets', async (req, res, next) => {});
 
-router.delete('/:id/savedBudgets/:id', async (req, res, next) => {
-    
+router.delete('/:id/savedBudgets/:budgetID', async (req, res, next) => {
+    try {
+        const id = req.params.budgetID;
+        const amt = await Budgets.remove(id);
+        res.status(200).json({ message: `You deleted ${amt} budgets.` });
+    } catch (err) {
+        next({ err, stat: 500, message: 'Error removing a specific budget.' });
+    }
 });
 
-router.put('/:id/savedBudgets/:id', async (req, res, next) => {});
+router.put('/:id/savedBudgets/:budgetID', async (req, res, next) => {});
 
 module.exports = router;

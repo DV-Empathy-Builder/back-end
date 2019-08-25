e44157cb1efdf0ebf0d5041cf346503af0578fde
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-08-25 13:11:24 -0400

    Create separate recurring and relocation tables; add .down

--- data/migrations/20190825120128__empathy01.js
+++ data/migrations/20190825120128__empathy01.js
@@ -8,47 +8,81 @@ exports.up = function(knex) {
                 .unique();
             users.string('password').notNullable();
         })
-        .createTable('categories', cats => {
-            cats.increments();
-            cats.string('category_name')
+        .createTable('categories', categories => {
+            categories.increments('category_id');
+            categories
+                .string('category_name')
                 .notNullable()
                 .unique();
         })
-        .createTable('budget_names', bname => {
-            bname.increments();
-            bname.string('budget_name').notNullable();
+        .createTable('budget_names', budget_names => {
+            budget_names.increments('budget_name_id');
+            budget_names.string('budget_name').notNullable();
+            budget_names.timestamps(true, true);
         })
-        .createTable('budget_items', bdgt => {
-            bdgt.increments('budget_item_id');
-            bdgt.integer('amount')
+        .createTable('recurring_budget', recurring_budget => {
+            recurring_budget.increments('recurring_item_id');
+            recurring_budget
+                .integer('amount')
                 .unsigned()
                 .notNullable()
                 .defaultTo(0);
-            bdgt.integer('user_id')
+            recurring_budget
+                .integer('user_id')
                 .unsigned()
                 .notNullable()
                 .references('user_id')
                 .inTable('users')
                 .onDelete('CASCADE')
-                .onUPDATE('CASCADE');
-            bdgt.integer('category_id')
+                .onUpdate('CASCADE');
+            recurring_budget
+                .integer('category_id')
                 .unsigned()
                 .notNullable()
                 .references('category_id')
                 .inTable('categories')
                 .onDelete('CASCADE')
-                .onUPDATE('CASCADE');
-            bdgt.integer('budget_name_id')
+                .onUpdate('CASCADE');
+        })
+        .createTable('relocation_budget', relocation_budget => {
+            relocation_budget.increments('recurring_item_id');
+            relocation_budget
+                .integer('amount')
+                .unsigned()
+                .notNullable()
+                .defaultTo(0);
+            relocation_budget
+                .integer('user_id')
+                .unsigned()
+                .notNullable()
+                .references('user_id')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            relocation_budget
+                .integer('category_id')
+                .unsigned()
+                .notNullable()
+                .references('category_id')
+                .inTable('categories')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            relocation_budget
+                .integer('budget_name_id')
                 .unsigned()
                 .notNullable()
                 .references('budget_name_id')
                 .inTable('budget_names')
                 .onDelete('CASCADE')
-                .onUPDATE('CASCADE');
-            bdgt.boolean('isRecurring')
-                .notNullable()
-                .defaultTo(1);
+                .onUpdate('CASCADE');
         });
 };
 
-exports.down = function(knex) {};
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('recurring_budget')
+        .dropTableIfExists('relocation_budget')
+        .dropTableIfExists('users')
+        .dropTableIfExists('categories')
+        .dropTableIfExists('budget_names');
+};
